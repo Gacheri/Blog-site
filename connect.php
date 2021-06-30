7b6c8f1102f64646b6d7432ad054a97dec4bd803
Gacheri <michenibrenda81@gmail.com>
2021-06-30 17:26:47 +0300

    database connection

--- connect.php
+++ connect.php
@@ -1,24 +1,12 @@
+
 <?php
 
-$severname="localhost";
-$username="root";
-$password="roots1";
-$dbname="tablet";
-
-$conn=mysqli_connect ($severname, $username, $password ,$dbname) or die ("unable to connect to host");
-//  $sql =mysqli_select_db ('$dbname',$conn) or die ("unable to connect to database");
-
-
-include "connect.php";
-if($conn->connect_error) {
-    die("connection failed: " . $conn->connect_error);
-}
-$query = "INSERT INTO article VALUES ('test','test1')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "record inserted successfully";
-} else{
-    echo "Error: " . $sql . "<br>" .$conn->error;
-}
-
+$dsn = 'mysql:host=localhost';
+$username= 'root';
+$password='roots1';
+$options=[];
+$conn=new PDO($dsn,$username,$password,$options);
+// if($conn=new PDO($dsn,$username,$password,$options)){
+//     echo"Your database connectionphp was successfull my Lord.";
+// }
 ?>
--- create.php
+++ create.php
@@ -1,4 +1,31 @@
-<!DOCTYPE html>
+
+<?php
+if (!empty($_POST)){
+     require 'connect.php';
+     $message='';
+
+     if(isset ($_POST['title']) && isset ($_POST['content'])){
+        try{
+            $title = $_POST['title'];
+            $content = $_POST['content'];
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ 
+            $sql = "INSERT INTO tablet.article (title,content) VALUES (:title, :content)";
+            $statement = $conn->prepare($sql);  
+         
+         if($statement->execute([':title' => $title, ':content' => $content ])){
+             echo 'Your majesty. Data has been successfully entered';
+          }
+        } catch(PDOException $e){
+            echo"Data not entered to database: " . $e->getMessage();
+        }
+ 
+         
+     } 
+}
+   
+
+?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -8,28 +35,18 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <div class='message-alert'>
+        <?php
+        if(!empty($message)):
+        ?>
+        <h1 class='message-alert-body'>
+            <?= $message; ?>
+        </h1>
+    </div>
+    <?php endif; ?>
    <div class='new-article'>
-        <form name="post" action="connect.php" method="POST" class='article-content' enctype="multipart/form-data">
-            <!-- <ul>
-                <li>
-                    <label for="title">Title</label>
-                    <input type="text" name='title' >
-                      
-                </li>
-
-                <li>
-                    <label for="">content</label>
-                    <input type="text" name='content' >      
-                </li>
-                <li>
-                    <button>Save to draft</button>
-                </li>
-                <li>
-                    <button>Publish</button>
-                </li>
-                
-            </ul>   -->
-        <label for="title" id='title-id'>
+        <form method="post" class='article-content' enctype="multipart/form-data">       
+        <label for="title" id='title-id' >
             Title
             <input type="text" name="title" placeholder="Enter your title here">
         </label>
@@ -38,7 +55,7 @@
             <textarea name="content" id="input-content" cols="30" rows="10"></textarea>
         </label>
         <button type='submit' id='draft'>Save to Drafts</button>
-        <button type="submit" id='publish'>Publish article</button>
+        <!-- <button type="submit" id='publish'>Publish article</button> -->
         </form>
    </div>
 </body>
